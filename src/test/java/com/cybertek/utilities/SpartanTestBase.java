7a01b82f7276930f2dddba4b9a12a08e0b299b7e
furkangergin <hunter_1325@hotmail.com>
2022-11-18 13:51:39 +0300

    initial day04

---
+++ src/test/java/com/cybertek/utilities/SpartanTestBase.java
@@ -0,0 +1,15 @@
+package com.cybertek.utilities;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public abstract class SpartanTestBase {
+
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://54.160.179.72:8000";
+    }
+}
